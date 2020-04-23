7a753ff73cab0bbd60fd955894865e8084f229af
Ismael Skywalker <ismael_junior@msn.com>
2020-04-23 17:46:53 -0300

    APP CRUD CHALLENGER 1
    Construção dos metodos cruds do primeiro desafio GoSTack RocketSeat

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,103 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  //retornar todos os repositorios criados
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  //capturar dados para objeto
+  const {title, url, techs} = request.body;
+
+  //criar objeto conforme as especificações do desafio
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  //adicionar novo objeto ao array
+  repositories.push(repository);
+
+  //retornar json do objeto
+  return response.json(repository);
+
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  //metodo para alterar repositorios
+  const { id } = request.params;
+  const {title, url, techs, likes} = request.body;
+
+  const repIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repIndex < 0){
+
+    return response.status(400).json({error: 'Repository not found!' });
+
+  }else{
+    
+    const repository = {
+      id: repositories[repIndex].id,
+      title,
+      url,
+      techs,
+      likes: repositories[repIndex].likes
+    };
+
+    
+    repositories[repIndex] = repository;
+
+    return response.json(repository);
+  }
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+
+  const repIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repIndex < 0){
+
+    return response.status(400).json({erro: 'repository not found!'});
+
+  }else{
+
+    repositories.splice(repIndex,1);
+
+    return response.status(204).send();
+  }
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const {id} = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository){
+
+    return response.status(400).json({error: 'Repository do not exists!'});
+
+  }else{
+
+    repository.likes += 1;
+
+    return response.json(repository);
+
+  }
+
+  
 });
 
 module.exports = app;
